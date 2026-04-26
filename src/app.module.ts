bdabe0ca2b558bc8a7069ac80623007456fee93f
MilSoltani <miladsoltani@outlook.de>
2026-04-26 15:08:53 +0200

    feat(modules): tickets and users modules with services implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UsersModule, TicketsModule],
   controllers: [],
   providers: [],
 })
---
+++ src/tickets/tickets.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from 'src/generated/prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { Ticket } from './entities/ticket.entity';
+
+@Injectable()
+export class TicketsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(
+    ticketWhereUniqueInput: Prisma.TicketWhereUniqueInput,
+  ): Promise<Ticket | null> {
+    return this.prisma.ticket.findUnique({
+      where: ticketWhereUniqueInput,
+    });
+  }
+
+  async findMany(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.TicketWhereUniqueInput;
+    where?: Prisma.TicketWhereInput;
+    orderBy?: Prisma.TicketOrderByWithRelationInput;
+  }): Promise<Ticket[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.ticket.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async create(data: Prisma.TicketCreateInput): Promise<Ticket> {
+    return this.prisma.ticket.create({
+      data,
+    });
+  }
+
+  async update(params: {
+    where: Prisma.TicketWhereUniqueInput;
+    data: Prisma.TicketUpdateInput;
+  }): Promise<Ticket> {
+    const { where, data } = params;
+    return this.prisma.ticket.update({
+      data,
+      where,
+    });
+  }
+
+  async delete(where: Prisma.TicketWhereUniqueInput): Promise<Ticket> {
+    return this.prisma.ticket.delete({
+      where,
+    });
+  }
+}
