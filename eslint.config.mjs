11201c997838cbcf37d41af472725f490302f801
MilSoltani <miladsoltani@outlook.de>
2026-05-10 10:22:09 +0200

    installed tanstack react query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,37 @@
 import antfu from '@antfu/eslint-config'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
 
-export default antfu({
-  type: 'app',
-  vue: false,
-  react: true,
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: false,
-    quotes: 'single',
-    jsx: true,
+export default antfu(
+  {
+    type: 'app',
+    vue: false,
+    react: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: false,
+      quotes: 'single',
+      jsx: true,
+    },
+    ignores: ['**/migrations/*'],
   },
-  ignores: ['**/migrations/*'],
-}, {
-  rules: {
-    'ts/consistent-type-definitions': ['error', 'type'],
-    'no-console': ['warn'],
-    'antfu/no-top-level-await': ['off'],
-    '@stylistic/jsx-max-props-per-line': ['error', { maximum: 1 }],
+
+  {
+    rules: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': ['off'],
+      '@stylistic/jsx-max-props-per-line': ['error', { maximum: 1 }],
+    },
   },
-})
+
+  {
+    plugins: {
+      '@tanstack/query': tanstackQuery,
+    },
+    rules: {
+      ...tanstackQuery.configs.recommended.rules,
+    },
+  },
+)
