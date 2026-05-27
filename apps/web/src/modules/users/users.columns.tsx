354d55cd060a2e60adcf9418354e45b7d2b88bd1
MilSoltani <miladsoltani@outlook.de>
2026-05-27 06:50:31 +0200

    inline actions as icons for table

--- apps/web/src/modules/users/users.columns.tsx
+++ apps/web/src/modules/users/users.columns.tsx
@@ -2,8 +2,7 @@ import type { User } from '@raven/api/exports'
 import type { ColumnDef } from '@tanstack/react-table'
 import type { useDeleteUser } from './hooks/users.hooks'
 import { Button } from '@raven/web/common/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@raven/web/common/components/ui/dropdown-menu'
-import { IconArrowsUpDown, IconDotsFilled } from '@tabler/icons-react'
+import { IconArrowsUpDown, IconTrash } from '@tabler/icons-react'
 
 export function createUsersColumns(deleteUser: ReturnType<typeof useDeleteUser>): ColumnDef<User>[] {
   return [
@@ -30,28 +29,17 @@ export function createUsersColumns(deleteUser: ReturnType<typeof useDeleteUser>)
     {
       header: 'actions',
       cell: ({ row }) => (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="ghost"
-              className="h-8 w-8 p-0"
-            >
-              <IconDotsFilled className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem
-              variant="destructive"
-              onClick={(e) => {
-                e.stopPropagation()
-                deleteUser.mutate(row.original.id)
-              }}
-            >
-              Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div>
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              e.stopPropagation()
+              deleteUser.mutate(row.original.id)
+            }}
+          >
+            <IconTrash className="h-4 w-4" />
+          </Button>
+        </div>
       ),
     },
   ]
