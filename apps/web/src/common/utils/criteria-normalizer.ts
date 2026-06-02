3b8f22e4dbd8a610d5896d7635bbdb6de49f190e
MilSoltani <miladsoltani@outlook.de>
2026-06-02 07:20:21 +0200

    bug fix in hooks

--- apps/web/src/common/utils/criteria-normalizer.ts
+++ apps/web/src/common/utils/criteria-normalizer.ts
@@ -1,11 +1,11 @@
 import type { Criteria } from '@xenon/api/exports'
 
 export function normalizeCriteria(criteria: Criteria | undefined) {
-	if (!criteria) return undefined
+	if (!criteria) return {}
 
 	return {
 		...criteria,
-		page: criteria.page ? String(criteria.page) : undefined,
-		limit: criteria.limit ? String(criteria.limit) : undefined,
+		page: criteria.page,
+		limit: criteria.limit,
 	}
 }
